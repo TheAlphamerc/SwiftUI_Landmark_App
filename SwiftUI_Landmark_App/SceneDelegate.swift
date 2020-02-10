b3ea29e788c99bc7e2b211e57aa4e58caae140c7
Sonu Sharma <sonu.sharma045@gmail.com>
2020-02-10 22:46:23 +0530

    Hexagon badge created

--- SwiftUI_Landmark_App/SceneDelegate.swift
+++ SwiftUI_Landmark_App/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: landmarkList.environmentObject(UserData()))
+            window.rootViewController = UIHostingController(rootView: Badge())
             self.window = window
             window.makeKeyAndVisible()
         }
---
+++ SwiftUI_Landmark_App/Supporting Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUI_Landmark_App
+//
+//  Created by Ashwin Das on 10/02/20.
+//  Copyright © 2020 Ashwin Das. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    var body: some View {
+        BadgeSymbol()
+        .padding()
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
