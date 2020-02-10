0f940fde3f3fadd58dc9ef749661d2e7052e67f5
Sonu Sharma <sonu.sharma045@gmail.com>
2020-02-10 13:21:52 +0530

    initial commit

---
+++ SwiftUI_Landmark_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Landmark_App
+//
+//  Created by Ashwin Das on 10/02/20.
+//  Copyright © 2020 Ashwin Das. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
