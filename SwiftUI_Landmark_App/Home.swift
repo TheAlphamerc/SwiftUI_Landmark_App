84331c03ea9dd4574b362c3fae383d48bfabc363
Sonu Sharma <sonu.sharma045@gmail.com>
2020-02-11 23:11:43 +0530

    featured landmark added on home page

--- SwiftUI_Landmark_App/Home.swift
+++ SwiftUI_Landmark_App/Home.swift
@@ -9,22 +9,39 @@
 import SwiftUI
 
 struct CategoryHome: View {
-    var  categories: [String: [Landmark]]{
+    var categories: [String: [Landmark]] {
         Dictionary(
             grouping: landmarkData,
-            by:{ $0.category.rawValue}
+            by: { $0.category.rawValue }
         )
     }
     
+    var featured: [Landmark] {
+        landmarkData.filter { $0.isFeatured }
+    }
+    
     var body: some View {
-        NavigationView{
-            List{
-                ForEach(categories.keys.sorted(), id: \.self){key in
-                    Text(key)
+        NavigationView {
+            List {
+                FeaturedLandmarks(landmarks: featured)
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                  .listRowInsets(EdgeInsets())
+                ForEach(categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: self.categories[key]!)
                 }
+                  .listRowInsets(EdgeInsets())
             }
-            .navigationBarTitle(Text("Fearured"))
+            .navigationBarTitle(Text("Featured"))
         }
+    }
+}
+
+struct FeaturedLandmarks: View {
+    var landmarks: [Landmark]
+    var body: some View {
+        landmarks[0].image.resizable()
     }
 }
 
---
+++ SwiftUI_Landmark_App/Supporting Views/CategoryRow.swift
@@ -0,0 +1,56 @@
+//
+//  CategoryRow.swift
+//  SwiftUI_Landmark_App
+//
+//  Created by Ashwin Das on 11/02/20.
+//  Copyright © 2020 Ashwin Das. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(self.categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(self.items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarkData[0].category.rawValue,
+            items: Array(landmarkData.prefix(4))
+        )
+    }
+}
