a48ef04a342baa116b4dd1f994be3ad10b5c96d9
Sonu Sharma <sonu.sharma045@gmail.com>
2020-02-12 07:40:49 +0530

    profile page controls added

--- SwiftUI_Landmark_App/Profile/ProfileHost.swift
+++ SwiftUI_Landmark_App/Profile/ProfileHost.swift
@@ -9,12 +9,34 @@
 import SwiftUI
 
 struct ProfileHost: View {
-  
+    @Environment(\.editMode) var mode
+    @EnvironmentObject var userData: UserData
     @State var draftProfile = Profile.default
 
     var body: some View {
         VStack(alignment: .leading,spacing: 20){
-              ProfileSummary(profile: draftProfile)
+            HStack{
+                if self.mode?.wrappedValue == .inactive{
+                    Button("Cancle"){
+                        self.draftProfile = self.userData.profile
+                        self.mode?.animation().wrappedValue = .inactive
+                    }
+                }
+                Spacer()
+                EditButton()
+            }
+            if self.mode?.wrappedValue == .inactive{
+                 ProfileSummary(profile: draftProfile)
+            }
+            else{
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear{
+                        self.draftProfile = self.userData.profile
+                }
+                .onDisappear{
+                    self.userData.profile = self.draftProfile
+                }
+            }
         }
     .padding()
     }
@@ -22,6 +44,6 @@ struct ProfileHost: View {
 
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileHost()
+        ProfileHost().environmentObject(UserData())
     }
 }
